1a7e4bb3531efa7e489bc8199052172dff8b6cb5
valentinM23 <85651668+valentinM23@users.noreply.github.com>
2024-09-20 16:09:23 -0300

    feat: subiendo actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,12 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+from machine import Pin
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('/index.html')
+
+
+app.run(port = 80)
